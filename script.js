c0574e4470630b6de4c230c6eed5c37650eb9963
Pedro A. da Silva <pedrosilvafsa12@gmail.com>
2023-06-14 17:04:58 -0300

    Make computer's choice more meaningful
    Changed the function that makes the choice for the computer so that it
    returns either "rock", "paper" or "scissors" instead of numbers.
    
    This way is easier to understand and also makes comparison with the
    player's choice simpler.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function computerSelection() {
-    return Math.floor(Math.random() * 3);
+let playerSelection = prompt("What will you play?", "");
+let computerSelection = getComputerChoice();
+
+// Choose the computer's option randomly (Rock: 0, Paper: 1, Scissors: 2)
+function getComputerChoice() {
+    let selected = Math.floor(Math.random() * 3);
+
+    if (selected === 0) {
+        selected = "rock";
+    } else if (selected === 1) {
+        selected = "paper";
+    } else {
+        selected = "scissors";
+    }
+
+    return selected;
 }
