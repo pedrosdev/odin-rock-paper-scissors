d8c9228450cfb6da139f7f6ead0214206d425ef0
Pedro A. da Silva <pedrosilvafsa12@gmail.com>
2023-06-14 18:16:12 -0300

    Implement multiple rounds and log results

--- script.js
+++ script.js
@@ -1,36 +1,63 @@
-// Get user's choice and make it case insensitive
-let playerSelection = prompt("What will you play?", "");
-playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+function game() {
+    // Keep track of the scores
+    let playerScore = 0;
+    let computerScore = 0;
 
-// Get computer's choice
-let computerSelection = getComputerChoice();
+    for (let i = 0; i < 5; i++) {
+        // Get user's choice and make it case insensitive
+        let playerSelection = prompt("What will you play?", "");
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 
-// Choose the computer's option randomly
-function getComputerChoice() {
-    let selected = Math.floor(Math.random() * 3);
+        // Get computer's choice
+        let computerSelection = getComputerChoice();
 
-    // Change value to the actual words
-    if (selected === 0) {
-        selected = "Rock";
-    } else if (selected === 1) {
-        selected = "Paper";
-    } else {
-        selected = "Scissors";
+        let roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult);
+
+        if (roundResult.includes("won")) {
+            playerScore++;
+        } else if (roundResult.includes("lost")) {
+            computerScore++;
+        }
     }
 
-    return selected;
-}
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return `${playerSelection} vs ${computerSelection}. That's a draw!`;
-    } else if (
-        playerSelection === "Rock" && computerSelection === "Scissors" ||
-        playerSelection === "Paper" && computerSelection === "Rock" ||
-        playerSelection === "Scissors" && computerSelection === "Paper"
-    ) {
-        return `${playerSelection} beats ${computerSelection}. You won!`;
+    if (playerScore > computerScore) {
+        console.log(`Congratulations! You won by ${playerScore} x ${computerScore}!`);
+    } else if (playerScore < computerScore) {
+        console.log(`Ha-ha! You lost by ${playerScore} x ${computerScore}!`);
     } else {
-        return `${computerSelection} beats ${playerSelection}. You lost!`;
+        console.log("A draw... Not that interesting, huh?");
+    }
+
+    // Choose the computer's option randomly
+    function getComputerChoice() {
+        let selected = Math.floor(Math.random() * 3);
+
+        // Change value to the actual words
+        if (selected === 0) {
+            selected = "Rock";
+        } else if (selected === 1) {
+            selected = "Paper";
+        } else {
+            selected = "Scissors";
+        }
+
+        return selected;
+    }
+
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            return `${playerSelection} vs ${computerSelection}. That's a draw!`;
+        } else if (
+            playerSelection === "Rock" && computerSelection === "Scissors" ||
+            playerSelection === "Paper" && computerSelection === "Rock" ||
+            playerSelection === "Scissors" && computerSelection === "Paper"
+        ) {
+            return `${playerSelection} beats ${computerSelection}. You won!`;
+        } else {
+            return `${computerSelection} beats ${playerSelection}. You lost!`;
+        }
     }
 }
+
+game();
