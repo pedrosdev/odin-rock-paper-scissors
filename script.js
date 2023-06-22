a9256765ecc345115faca60a7e03476bb5d8723b
Pedro A. da Silva <pedrosilvafsa12@gmail.com>
2023-06-22 17:39:27 -0300

    Add UI to the page
    Forgot to commit in the process, so the commit ended up becoming not
    atomic at all. Will try to do it properly next time.

--- script.js
+++ script.js
@@ -1,76 +1,97 @@
-// Implement a 5 round game of Rock Paper Scissors
-function game() {
-    // Keep track of the scores
-    let playerScore = 0;
-    let computerScore = 0;
+const options = document.querySelectorAll(".option");
+const resultWindow = document.getElementById("result-window");
 
-    for (let i = 0; i < 5; i++) {
-        // Get user's choice and make it case insensitive
-        let playerSelection = prompt("What will you play?", "");
-        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+options.forEach((option) => {
+  option.addEventListener("click", playRound)
+});
 
-        // Get computer's choice
-        let computerSelection = getComputerChoice();
-
-        // Get result of the current round
-        let roundResult = playRound(playerSelection, computerSelection);
-
-        // Print result message
-        if (roundResult === 1) {
-            console.log(`${playerSelection} beats ${computerSelection}. You won!`);
-            playerScore++;
-        } else if (roundResult === 0) {
-            console.log(`${playerSelection} vs ${computerSelection}. That's a draw!`);
-        } else {
-            console.log(`${computerSelection} beats ${playerSelection}. You lost!`);
-            computerScore++;
-        }
-    }
-
-    printGameResult(playerScore, computerScore);
-}
-
-game();
+let playerScore = 0;
+let computerScore = 0;
 
 // Choose the computer's option randomly
 function getComputerChoice() {
-    let selected = Math.floor(Math.random() * 3);
+  let selected = Math.floor(Math.random() * 3);
 
-    // Change value to the actual words
-    if (selected === 0) {
-        return "Rock";
-    } else if (selected === 1) {
-        return "Paper";
-    } else {
-        return "Scissors";
-    }
+  return ["Rock", "Paper", "Scissors"][selected];
 }
 
 // Compute the result of a single round of Rock Paper Scissors
-function playRound(playerSelection, computerSelection) {
-    if (
-        playerSelection === "Rock" && computerSelection === "Scissors" ||
-        playerSelection === "Paper" && computerSelection === "Rock" ||
-        playerSelection === "Scissors" && computerSelection === "Paper"
-    ) {
-        // Player won
-        return 1;
-    } else if (playerSelection === computerSelection) {
-        // Draw
-        return 0;
-    } else {
-        // Player lost
-        return -1;
-    }
+function playRound(e) {
+  // Get computer's choice
+  let computerSelection = getComputerChoice();
+
+  // Get player's choice
+  let playerSelection = e.target.textContent;
+
+  // Determine the winner for the round
+  let roundResult = getRoundResult(playerSelection, computerSelection);
+
+  if (updateScores(roundResult) === 5) {
+    finishGame();
+  } else {
+    showRoundResult(playerSelection, computerSelection, roundResult);
+  }
+}
+
+function getRoundResult(playerSelection, computerSelection) {
+  if (
+    playerSelection === "Rock" && computerSelection === "Scissors" ||
+    playerSelection === "Paper" && computerSelection === "Rock" ||
+    playerSelection === "Scissors" && computerSelection === "Paper"
+  ) {
+    // Player won
+    return 1;
+  } else if (playerSelection === computerSelection) {
+    // Draw
+    return 0;
+  } else {
+    // Player lost
+    return -1;
+  }
+}
+
+function updateScores(roundResult) {
+  if (roundResult === 1) {
+    playerScore++;
+  } else if (roundResult === -1) {
+    computerScore++;
+  }
+
+  // Show current scores
+  const scoresContainer = document.getElementById("scores");
+  scoresContainer.textContent = `You | ${playerScore} x ${computerScore} | Computer`;
+
+  return (playerScore > computerScore) ? playerScore : computerScore;
+}
+
+function showRoundResult(playerSelection, computerSelection, roundResult) {
+  let message = '';
+
+  if (roundResult === 1) {
+    message = `${playerSelection} beats ${computerSelection}. You won!`;  
+  } else if (roundResult === 0) {
+    message = `${playerSelection} vs ${computerSelection}. That's a draw!`;
+  } else {
+    message = `${computerSelection} beats ${playerSelection}. You lost!`;
+  }
+
+  resultWindow.textContent = message;
 }
 
 // Print the result of a game to the console
-function printGameResult(playerScore, computerScore) {
-    if (playerScore > computerScore) {
-        console.log(`Congratulations! You won by ${playerScore} x ${computerScore}!`);
-    } else if (playerScore < computerScore) {
-        console.log(`Ha-ha! You lost by ${playerScore} x ${computerScore}!`);
-    } else {
-        console.log("A draw... Not that interesting, huh?");
-    }    
+function finishGame() {
+  let message = '';
+  if (playerScore > computerScore) {
+    message = `Congratulations! You won!`;
+  } else if (playerScore < computerScore) {
+    message = `Ha-ha! You lost!`;
+  } else {
+    message = "A draw... Not that interesting, huh?";
+  }
+
+  resultWindow.textContent = message;
+
+  // Reset scores after end of each game
+  playerScore = 0;
+  computerScore = 0;
 }
